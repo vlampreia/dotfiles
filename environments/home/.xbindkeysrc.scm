0514a1c853e50f1aaa12ba43ec75438a1b5f37a6
vlampreia <victorlampreia@gmail.com>
2015-08-22 01:46:27 +0100

    Add MPD shortcuts to xbindkeys

--- environments/home/.xbindkeysrc.scm
+++ environments/home/.xbindkeysrc.scm
@@ -1,3 +1,7 @@
 (xbindkey '(XF86MonBrightnessUp) "xbacklight -inc 20")
 (xbindkey '(XF86MonBrightnessDown) "xbacklight -dec 20")
 (xbindkey '(XF86Tools) "$HOME/dotfiles/scripts/toggletouchpad.sh")
+(xbindkey '(Control+Alt + Next) "mpc next")
+(xbindkey '(Control+Alt + Prior) "mpc prev")
+(xbindkey '(Control+Alt + End) "mpc toggle")
+(xbindkey '(Control+Alt + Home) "mpc stop")
